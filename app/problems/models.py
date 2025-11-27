08ec79b4e29c607537d916edf50cc682b4f80f03
abdurauf0788 <turkeynumber063@gmail.com>
2025-11-27 23:30:21 +0500

    submitted_at models

--- app/problems/models.py
+++ app/problems/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from app.contests.models import Contest
 
-# Create your models here.
+class Problem(models.Model):
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    code = models.CharField(max_length=20)
+    max_score = models.IntegerField(default=100)
+    time_limit_ms = models.IntegerField(default=1000)
+    memory_limit_kb = models.IntegerField(default=65536)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    class Meta:
+        unique_together = ('contest', 'code')
+
+
--- app/submissions/models.py
+++ app/submissions/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from app.contests.models import Contest
+from app.coders.models import Coder
+from app.problems.models import Problem
 
-# Create your models here.
+
+class Submission(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('wrong_answer', 'Wrong Answer'),
+        ('runtime_error', 'Runtime Error'),
+        ('time_limit', 'Time Limit Exceeded'),
+        ('compilation_error', 'Compilation Error'),
+        ('partial', 'Partial Score'),
+    ]
+    contest = models.ForeignKey(Contest, on_delete=models.PROTECT)
+    problem = models.ForeignKey(Problem, on_delete=models.PROTECT)
+    coder = models.ForeignKey(Coder, on_delete=models.PROTECT)
+    language = models.CharField(max_length=50)
+    code = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES, default="pending")
+    score = models.IntegerField(default=0)
+    attempt_no = models.IntegerField(default=1)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    judged_at = models.DateTimeField(null=True, blank=True)
+
+
+    class Meta:
+        ordering = ["-submitted_at"]
+    
+
+    def __str__(self):
+        return f"{self.id}.{self.coder.nickname}  -- -- -- {self.language}"
