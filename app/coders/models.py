a30953708b58b7c8a84fd06f5533e2c6819d633a
abdurauf0788 <turkeynumber063@gmail.com>
2025-11-27 16:14:20 +0500

    coder models

--- app/coders/models.py
+++ app/coders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Coder(models.Model):
+    nickname = models.CharField(max_length=50, unique=True)
+    display_name = models.CharField(max_length=120, unique=True)
+    country = models.CharField(max_length=50)
+    bio = models.TextField(null=True, blank=True)
+    rating = models.IntegerField(default=1500)
+    points_total = models.IntegerField(default=0)
+    total_submissions = models.IntegerField(default=0)
+    accepted_submissions = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}. {self.nickname} -- -- -- {self.country}"
